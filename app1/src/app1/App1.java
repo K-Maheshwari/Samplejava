1df2be08c80f565505cc567b269278f315fab8c3
K-Maheshwari <balamaheshsk@gmail.com>
2023-07-22 11:43:14 +0530

    I am Mahesh.

--- app1/src/app1/App1.java
+++ app1/src/app1/App1.java
@@ -1,22 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package app1;
 
-/**
- *
- * @author acer
- */
+import java.util.Scanner;
+
 public class App1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("hello world");
+        Scanner in=new Scanner(System.in);
+        int year;
+        System.out.println("Enter the year : ");
+        year=in.nextInt();
+        if (year % 4 == 0 && (year % 100 == 0 && year % 400 == 0)) {
+            System.out.println("It is leap year");
+        } else {
+            System.out.println("It is not a leap year");
+        }
     }
-    
+
 }
